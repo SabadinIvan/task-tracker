8ba326c941a8f264456be21fd1e06df4cec5b962
IvanSabadin <sabadin.ivan.84@gmail.com>
2026-02-12 15:12:48 +0300

    fixed task status enum

--- task-spring-app/src/main/java/com/sabadin/taskspringapp/task/model/entity/TaskStatus.java
+++ task-spring-app/src/main/java/com/sabadin/taskspringapp/task/model/entity/TaskStatus.java
@@ -1,11 +1,32 @@
 package com.sabadin.taskspringapp.task.model.entity;
 
+import jakarta.annotation.Nonnull;
+
 public enum TaskStatus {
-    NEW,
-    WAIT,
-    IN_PROGRESS,
-    IN_TEST,
-    DONE,
-    REPORTED,
-    ARCHIVE
+    NEW("new"),
+    WAIT("wait"),
+    IN_PROGRESS("in_progress"),
+    IN_TEST("in_test"),
+    DONE("done"),
+    REPORTED("reported"),
+    ARCHIVE("archive");
+
+    private final String title;
+
+    TaskStatus(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public static TaskStatus from(@Nonnull String title) {
+        for (TaskStatus value : values()) {
+            if (value.getTitle().equals(title)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException(title + "can not be converted to " + TaskStatus.class.getSimpleName());
+    }
 }
