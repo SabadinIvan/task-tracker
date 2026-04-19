b29f2f15dfbfb5969f3e6b393e6cb198e5d11fc7
IvanSabadin <sabadin.ivan.84@gmail.com>
2026-04-19 04:58:00 +0300

    add header constants

---
+++ task-spring-app/src/main/java/com/sabadin/taskspringapp/common/constants/HeaderConstants.java
@@ -0,0 +1,8 @@
+package com.sabadin.taskspringapp.common.constants;
+
+public class HeaderConstants {
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    public static final String HEADER_CONTENT_TYPE = "Content-Type";
+    public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    public static final String HEADER_REFRESH_TOKEN = "Refresh-Token";
+}
